39ece09fe5af189cb8ce46ed1669cb21a1623743
namaymahindru <namaym9@gmail.com>
2024-01-07 00:57:45 +0530

    ..

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -3,8 +3,15 @@ import './DescriptionBox.css'
 
 const DescriptionBox = () => {
   return (
-    <div className='descriptionbox'>
-      
+      <div className='descriptionbox'>
+          <div className="descriptionbox-navigator">
+              <div className="description-nav-box">Description</div>
+              <div className='description-nav-box fade'>Reviews(122)</div>
+
+          </div>
+          <div className="descriptionbox-description">
+              
+      </div>
     </div>
   )
 }
