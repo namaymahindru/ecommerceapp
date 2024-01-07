04e027835374bb0a33adc2aab666954e1dba1d3a
namaymahindru <namaym9@gmail.com>
2024-01-08 01:08:11 +0530

    Added some imp line in ShopContext page

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from "react";
 import all_product from '../Components/Assets/all_product'
 
-export const ShopContext = createContext(null);
+export const ShopContext = createContext(null); // Created a context of data that can be used by any component and no use tot pass the data through props
 const getDefaultCart = () =>
     {
         let cart = {};
