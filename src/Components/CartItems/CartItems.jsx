3f3ec06b6f318c0691e3c4f85c3e7ec91fa20f92
namaymahindru <namaym9@gmail.com>
2024-01-11 00:30:55 +0530

    changes made

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -23,20 +23,43 @@ const CartItems = () => {
           {
               if (cartItems[e.id] > 0)
               {
-                        <div>
-            <div className="cartitems-format">
+                      return  <div>
+            <div className="cartitems-format cartitems-format-main">
                         <img src={e.image} alt="" className='carticon-product-icon' />
                                 <p>{e.name}</p>
                                 <p>{e.new_price}</p>
-                        <button className='cartitems-quantity'>cartItems[e.id]</button>
-                                <p>{e.new_price*cartItems[e.id]}</p>
-                        <img src={remove_icon} onClick={() =>{removeFromCart(e.id)} } alt="" />
+                          <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                                <p>${e.new_price*cartItems[e.id]}</p>
+                        <img  className='cartitems-remove-icon'src={remove_icon} onClick={() =>{removeFromCart(e.id)} } alt="" />
                 </div>
                     <hr/>
                 </div>
-                  }
+            }
+            return null;
               
-        })}
+          })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>Cart Total</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Total</p>
+              <p>${0}</p>
+            </div>
+            <hr />
+            <div className='cartitems-total-item'> 
+              <p>Shipping Fees</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <h4>Total</h4>
+              <h4>${0}</h4>
+            </div>
+          </div>
+          <button>Proceed to Checkout</button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -62,3 +62,4 @@ const ProductDisplay = (props) => {
 }
 
 export default ProductDisplay
+
